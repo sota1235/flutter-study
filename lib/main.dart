292b79db4306694aa10f4e7c343334f9c13c7191
sota1235 <sota1235@gmail.com>
2021-08-26 16:49:32 +0900

    modify: use external libs

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,14 +7,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return MaterialApp(
         title: 'Flutter Demo',
         home: Scaffold(
             appBar: AppBar(
-              title: const Text('Welcome to Flutter'),
+              title: Text('Welcome to Flutter'),
             ),
-            body: const Center(
-              child: const Text('Hello word'),
+            body: Center(
+              child: Text(wordPair.asPascalCase),
             )));
   }
 }
