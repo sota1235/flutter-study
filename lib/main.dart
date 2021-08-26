7339ea9a146d1e8274af658007594444d5d1da96
sota1235 <sota1235@gmail.com>
2021-08-26 16:56:50 +0900

    modify: use stateflu widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,22 @@ class MyApp extends StatelessWidget {
               title: Text('Welcome to Flutter'),
             ),
             body: Center(
-              child: Text(wordPair.asPascalCase),
+              child: RandomWords(),
             )));
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
